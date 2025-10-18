3f486cdf1a795ebf52f00c4dc90631974c7bbb30
LucciCoffeeTime <dawnbudge@live.com>
2025-10-18 18:37:34 -0400

    Learned a bunch of stuff and ignored a few things

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,14 +1,26 @@
 
 
+// if variable starts with capital its a class
 
 
-
-#include <iostream>
-
-using namespace std;
+#include <iostream> // This is the library required for cout and cin
 
 int main() {
-	cout << "Hello, World!" << endl;
+	int x = 1;  // integer type whole number
+	double y = 2.5;  // is a float
+	char z = 'A';  // single quotes for single characters
+	std::string str = "This is a string"; // stores text
+	bool isTrue = true;  // true or false value
+
+	std::cout << "enter a number: " ; // consol output   print() python
+	std::cin >> x;               // consol input    input() python
+
+	std::cout << "You entered: " << x << std::endl; // std::endl is like \n new line
+
+
+
+	std::cout << "Hello, World!" << std::endl;
+	std::cin >> x;
 	return 0;
 }
 
