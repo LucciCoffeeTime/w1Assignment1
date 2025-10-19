fe95b6f25b66c2f75a259537bd1fd6c25b4348f5
LucciCoffeeTime <dawnbudge@live.com>
2025-10-18 22:38:44 -0400

    Fully restructured the program and made one suitable to my hobby/interest

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,37 +1,64 @@
 
 
-// if variable starts with capital its a class
-
-
 #include <iostream> // This is the library required for cout and cin
 
 int main() {
-	int x = 1;  // integer type whole number
-	double y = 2.5;  // is a float
-	char z = 'A';  // single quotes for single characters
-	std::string str = "This is a string"; // stores text
-	bool isTrue = true;  // true or false value
 
-	std::cout << "enter a number: " ; // consol output   print() python
-	std::cin >> x;               // consol input    input() python
+	using namespace std;			// allows us to use cout and cin without std::
 
-	std::cout << "You entered: " << x << std::endl; // std::endl is like \n new line
+	string game;						// this variable stores the name of the game
+	string enemyrace;					// this variable stores the name of the enemy
+	string enemyclassification;  		// this variable stores the classification of the enemy	
+	string attack1;  					// this variable stores the first action of the enemy
+	string attack2;  					// this variable stores the second action of the enemy
+	string attack3;  					// this variable stores the third action of the enemy
+	string attack4;  					// this variable stores the fourth action of the enemy
+	string attack5;  					// this variable stores the fifth action of the enemy
+	string attack6;  					// this variable stores the sixth action of the enemy
+	string attack7;  					// this variable stores the seventh action of the enemy
 
+	// this section instructs the user to input the required information and avoid using spaces
+	cout << "Welcome to my enemy action documentor. DO NOT USE SPACES - when answering any question" << endl;
+	cout << " " << endl;
 
+	// this section collects the game being played from the user
+	cout << "What game are you playing?: ";
+	cin >> game; 
+	cout << " " << endl;
 
-	std::cout << "Hello, World!" << std::endl;
-	std::cin >> x;
+	// this section collects the enemy race from the user
+	cout << "What enemy are you facing?: ";
+	cin >> enemyrace; 
+	cout << " " << endl;
+
+	// this section collects the enemy classification from the user
+	cout << "What is the classification of this enemy: BOSS, ELITE, MAJOR, MINOR?: ";
+	cin >> enemyclassification;
+	cout << " " << endl;
+
+	// this section collects 7 actions of the enemy from the user
+	cout << "Please enter 7 - ACTIONS - the enemy has been seen doing." << endl;
+	cin >> attack1; 
+	cin >> attack2; 
+	cin >> attack3; 
+	cin >> attack4; 
+	cin >> attack5; 
+	cin >> attack6; 
+	cin >> attack7; 
+	cout << " " << endl;
+	cout << " " << endl;  // double spacing for readability
+
+	// this section outputs all the collected information back to the user
+	cout << "Thank you for the information." << endl;
+	cout << " " << endl;
+	cout << "You are playing: " << game << endl;
+	cout << "You are fighting against a: " << enemyrace << endl; 
+	cout << "It's classification is: " << enemyclassification << endl;
+	cout << "This enemy has the following actions: " << endl;
+	cout << attack1 << ", " << attack2 << ", " << attack3 << ", " << attack4 << ", " << attack5 << ", " << attack6 << ", " << attack7 << endl;	
+	cout << " " << endl;
+	cout << "This information will be documented!" << endl;  // this is a pseudo-confirmation message
+
 	return 0;
 }
 
-
-
-
-
-
-// chapter 5
-// chapter 2
-// comments are ignored by the compiler
-// single line comment
-// CHAPTER 3
-// CHAPTER 4
